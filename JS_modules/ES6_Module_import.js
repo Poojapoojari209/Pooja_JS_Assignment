77a49afb0b540af28cd986155e42de804fd8ca44
Poojapoojari209 <poojapoojari2001@gmail.com>
2025-11-06 22:18:51 +0530

    ES6 Module

---
+++ JS_modules/ES6_Module_import.js
@@ -0,0 +1,11 @@
+//main.js  ES6 modules
+
+import {add, subtraction} from './ES_Module_export.js';
+import multiply from './ES_Module_export.js';
+
+console.log("Addition", add(5, 3));
+console.log("Subtract", subtraction(10, 4));
+// console.log("PI value:", PI);
+console.log("Multiplication:", multiply(7, 2));
+
+
---
+++ JS_modules/ES_Module_export.js
@@ -0,0 +1,18 @@
+//MathOperation.js ES6 modules
+export function add(a, b){
+    return a + b;
+}
+
+export function subtraction(a, b){
+    return a - b;
+}
+
+ export const PI = 3.14159;
+
+export default function multiply(a, b){
+    return a * b;
+}
+
+
+
+
