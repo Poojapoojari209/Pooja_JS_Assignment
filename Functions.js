56b7d6eb4def2a011790399a2fa7e2fb6f123588
Poojapoojari209 <poojapoojari2001@gmail.com>
2025-11-07 07:17:29 +0530

    Arrow Function and function Expression

---
+++ Functions.js
@@ -0,0 +1,10 @@
+// Traditional function expression
+const addTraditional = function(a, b) {
+  return a + b;
+};
+
+// Arrow function 
+const addArrow = (a, b) => a + b;
+
+console.log(addTraditional(5, 3)); 
+console.log(addArrow(5, 3));     
