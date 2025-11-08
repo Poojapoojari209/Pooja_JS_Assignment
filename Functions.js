b57b9c91bff623f1c073ce6ba4dc16422d1d1d97
Poojapoojari209 <poojapoojari2001@gmail.com>
2025-11-08 07:22:38 +0530

    types of function

--- Functions.js
+++ Functions.js
@@ -1,10 +1,38 @@
 // Traditional function expression
-const addTraditional = function(a, b) {
-  return a + b;
+const addTraditional = function(p, q) {
+  return p + q;
 };
 
 // Arrow function 
 const addArrow = (a, b) => a + b;
 
 console.log(addTraditional(5, 3)); 
-console.log(addArrow(5, 3));     
+console.log(addArrow(5, 3));  
+
+
+// Anonymous Function Assigned to a Variable
+const sayHello = function() {
+  console.log("Hello from an anonymous function!");
+};
+
+sayHello(); 
+
+
+
+//Anonymous Function as a Callback(with setTimeout)
+setTimeout(function() {
+  console.log("This message appears after 2 seconds.");
+}, 2000);
+
+//Anonymous Arrow Function
+const multiply = (x, y) => x * y;
+
+console.log(multiply(5, 3)); 
+
+
+
+//immediate anonymous function
+(function() {
+  var message = "This is an immediate anonymous function!";
+  console.log(message);
+})();
