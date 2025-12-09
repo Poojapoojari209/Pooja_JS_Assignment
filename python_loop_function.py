f62882dec8beb796dd648c858daa8498472cb289
Poojapoojari209 <poojapoojari2001@gmail.com>
2025-12-09 22:11:37 +0530

    list and string Greeks for greeks problem

--- python_loop_function.py
+++ python_loop_function.py
@@ -96,3 +96,131 @@ class Solution:
         self.printGfg(n-1)
         
 
+# List Traversal
+# You are given a list that contains integers. You need to print the elements of the list with a space between them.
+def listTraversal(arr):
+    #code here
+    for i in arr:
+        print(i, end=" ")
+
+
+# Length of The List
+# You are given a list that contains integers. You need to return the length of the list.
+def listLength(arr):
+    return(len(arr))#output:5
+
+# Sum The List : You are given a list that contains integers. You need to return the sum of the list.
+def listSum(arr):
+    #code here
+    # return sum(arr)
+    sum = 0
+    for i in arr:
+        sum += i
+    return sum
+    
+
+# Decrement List Values : You are given a list that contains integers. You need to decrement each element of the list by 1 and return the list.
+def decrementList(arr):
+    #code here
+    list=[]
+    for i in arr:
+        j=i-1
+        list.append(j)
+    return list
+
+# Append To List
+# You are given three inputs a, b, c. You need to create a list and append a, b, c to the list and then return that list.
+def appendToList(a,b,c):
+    #code here
+    return [a,b,c]
+
+
+# Less Than - You are given a number k and a list arr[] that contains integers. You need to return list of numbers that are less than k.
+def lessThan(arr, k):
+    #code here
+    ans = [i for i in arr if i<k]
+    return ans #2 1 5
+
+
+# Average - You are given a list arr that contains integers. You need to return average of the non negative integers.
+def nonNegativeAverage(arr):
+    
+    li = [i for i in arr if i>=0]
+    avg = sum(li)/len(li)
+    return avg
+
+
+# Repeat the Strings - Python
+# Given two strings a and b. The task is to make a new string where the string with longer length should be in between and the one with shorter length should be outside on front and end. New string should be like shorter+longer+shorter.
+# Function to join given strings
+def combo_string(a, b):
+
+    # your code here
+    short = a
+    longer = b
+    if(len(a) > len(b)):
+      short = b
+      longer = a
+    return short + longer + short # forkpythonfork
+
+# String Functions I - Python
+# In this question, we'll take a look at inbuilt string methods like title(), swapcase(), find(), strip().
+def trim(str):
+     return str.strip()
+
+def exists(str, x):
+    return str.find(x)
+
+def titleIt(str):
+    return str.title()
+
+def casesSwap(str):
+    return str.swapcase()
+
+# hello
+# 2
+# Hello
+# HELLO
+
+# Convert String to LowerCase : Given a string s. The task is to convert string characters to lowercase.
+class Solution:
+    def toLower (self , s : str)-> str :
+        #code here 
+        return s.lower() #abcdde
+    
+# Reverse String : Given a string s, you need to reverse it.
+    # def reverseString(s):
+    # #code here
+    # result = ""
+    # for i in range(len(s) - 1, -1, -1):
+    #     result = result + s[i]
+    # return result #olleH
+
+# Check Palindrome : Given a string s, you need to check if it is palindrome or not. A palidrome is a string that reads the same from front and back.
+def isPalindrome(s):
+    #code here
+    if s.lower() == s[::-1].lower():
+        return True
+    else:
+        return False
+    # False
+
+
+# Find Pattern
+# Given a string s, and a pattern p. You need to find if p exists in s or not and return the starting index of p in s. If p does not exist in s then -1 will be returned. Here p and s both are case-sensitive.
+def findPattern(s,p):
+    #code here
+       for i in range(0, len(s) - len(p) + 1):
+        if s[i:i + len(p)] == p:
+            return i
+        return -1 #2
+       
+# Decimal number to binary number
+# Given a decimal number n (positive) in string format, compute its binary string equivalent and return it.
+def toBinary(n):
+    #Your code here
+    binary = ""
+    while n > 0:
+        binary += str(n%2)
+        n = int(n/2)
+    return binary[::-1] 
