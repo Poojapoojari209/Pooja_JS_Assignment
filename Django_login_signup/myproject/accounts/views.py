b1626e0ba302a42cdaa0cc40c2f30ee2bb4f00a3
Poojapoojari209 <Poojapoojari2001@gmail.com>
2025-12-28 22:10:22 +0530

    Django signup, login and profile simple program

--- Django_login_signup/myproject/accounts/views.py
+++ Django_login_signup/myproject/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
@@ -26,3 +27,7 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('login')
+
+@login_required(login_url='login')
+def profile(request):
+    return render(request, 'profile.html')
