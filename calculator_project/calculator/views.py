fa93909fbbd997fd739d3c416c153e5ee461dfc2
Poojapoojari209 <Poojapoojari2001@gmail.com>
2026-01-02 21:12:40 +0530

    django calculator simple program

--- calculator_project/calculator/views.py
+++ calculator_project/calculator/views.py
@@ -4,8 +4,8 @@ def add(request):
     result = None
 
     if request.method == 'POST':
-        num1 = int(request.POST.get('num1'))
-        num2 = int(request.POST.get('num2'))
+        num1 = int(request.POST.get('number1'))
+        num2 = int(request.POST.get('number2'))
         result = num1 + num2
 
     return render(request, 'add.html', {'result': result})
