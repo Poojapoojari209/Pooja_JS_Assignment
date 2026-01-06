7c020231360f99b42859631073f498bee8f8a592
Poojapoojari209 <Poojapoojari2001@gmail.com>
2026-01-06 21:33:53 +0530

    simple flask example

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Welcome to Flask!"
+    return "Hello, welcome to flask"
 
 @app.route('/about')
 def about():
-    return "This is About Page"
+    return "An About page paragraph should be a concise, engaging summary that tells your brand's story, explains what you do and who you serve, and builds trust with your audience "
 
 if __name__ == '__main__':
     app.run(debug=True)
