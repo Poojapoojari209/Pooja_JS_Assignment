df4ea820bcf086ddf8517bc5a50c5f7990083c62
Poojapoojari209 <Poojapoojari2001@gmail.com>
2026-01-07 21:28:34 +0530

    simple flask program

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,12 @@ def home():
 @app.route('/about')
 def about():
     return "An About page paragraph should be a concise, engaging summary that tells your brand's story, explains what you do and who you serve, and builds trust with your audience "
+
+@app.route('/add')
+def add():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    return f"Sum is {a + b}"
 
 if __name__ == '__main__':
     app.run(debug=True)
